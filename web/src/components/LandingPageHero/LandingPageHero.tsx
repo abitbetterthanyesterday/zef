0f014fe9434d7bf65ae51e3271d7a589db63f367
Aloys Berger <alo@reaktive.com>
2022-10-24 22:30:25 +1000

    feat(Landing-page): Add logo to the landing page
    Add logo (WIP) to the landing page and start positioning it (rought first pass!)

--- web/src/components/LandingPageHero/LandingPageHero.tsx
+++ web/src/components/LandingPageHero/LandingPageHero.tsx
@@ -2,35 +2,55 @@ import {
   Box,
   Button,
   Flex,
+  Heading,
   Input,
   InputGroup,
   InputRightElement
 } from '@chakra-ui/react'
+
+import LogoV1 from '../LogoV1/LogoV1'
 const LandingPageHero = () => {
   return (
     <Box as="section">
       <Flex
         height={'100vh'}
-        direction="column"
-        justifyContent={'center'}
-        alignItems={'center'}
         paddingBottom={16}
-        gap={8}
         paddingX={8}
+        position="relative"
       >
-        <Box background={'yellow.200'} height={80} width={80} />
-        <InputGroup>
-          <Input borderRadius={'xl'} border={'2px'} borderColor={'gray.900'} />
-          <InputRightElement>🔍</InputRightElement>
-        </InputGroup>
-        <Button
-          size="lg"
-          leftIcon={<span>🚀</span>}
-          border={'2px'}
-          backgroundColor={'yellow.300'}
+        <Flex
+          zIndex={1}
+          flexGrow={1}
+          direction="column"
+          justifyContent={'center'}
+          alignItems={'center'}
+          gap={8}
         >
-          Post an ad
-        </Button>
+          <Heading size="4xl">Zefo</Heading>
+          <Heading size="2xl">
+            The zero bullsh*t paragliding marketplace
+          </Heading>
+          <InputGroup>
+            <Input
+              borderRadius={'xl'}
+              border={'2px'}
+              borderColor={'gray.900'}
+            />
+            <InputRightElement>🔍</InputRightElement>
+          </InputGroup>
+          <Button
+            shadow="sm"
+            size="lg"
+            leftIcon={<span>🚀</span>}
+            border={'2px'}
+            backgroundColor={'yellow.300'}
+          >
+            Post an ad
+          </Button>
+        </Flex>
+        <Box height={80} width={80} position="absolute" zIndex={0} bottom={12}>
+          <LogoV1 />
+        </Box>
       </Flex>
     </Box>
   )
